d0cf95b8799a8c2dad298a7469be40d961db2b87
raylu15742cs <raylucs15742@gmail.com>
2022-10-03 10:21:36 -0700

    intro to typescript

--- example.ts
+++ example.ts
@@ -1,12 +1,110 @@
 // Type Annotations
 
+// let myName: string = "CJ";
+// let isCool: boolean = true;
+// let favNumber: number = 42;
+
+// function add(a: number,b: number) {
+//     return a + b;
+// }
+
+// console.log(add(40, 2));
+
 // Inferred Types
 
+let myName = "CJ";
+let isCool = true;
+let favNumber = 42;
+
+function add(a: number,b: number) {
+    return a + b;
+}
+
+let result = add(40, 2) 
+
+console.log(result);
+
+const numbers = [4, 7, 13, 42];
+
+// numbers.push("wow");
+
 // Union Types
+const numbersAndStrings: (string | number)[] = ["Panzer", 4, "CJ", 7, 13, 42];
+
+let theAnswer: string | number = 42;
+theAnswer = "42";
+
+console.log(numbersAndStrings)
 
 // Objects and Interfaces
 
+// interface Person {
+//     name: string;
+//     favNumber: number;
+// }
+
+// function greet(person: Person) {
+//     return `Hello ${person.name}`
+// }
+
+// console.log(greet({
+//     name: "CJ",
+//     favNumber: 42
+// }));
+
 // Optional Properties
+type StreamerType = "affiliate" | "partner";
+
+
+interface Person {
+    name: string;
+    favNumber: number;
+    dogName? : string;
+    streamerType: StreamerType;
+    currentTime?() : Date;
+}
+
+// type Person = {
+//     name: string;
+//     favNumber: number;
+//     dogName?: string;
+// }
+
+function greet(person: Person) {
+    if(person.currentTime) {
+        return `Hello ${person.name} it is ${person.currentTime()}`
+    }
+    return `Hello ${person.name}`;
+}
+
+console.log(greet({
+    name: "CJ",
+    favNumber: 42,
+    streamerType: "partner",
+    currentTime() {
+        return new Date();
+    }
+}));
+
+function greetPersonAndDog(person: Person) {
+    if (person.dogName) {
+        return `Hello ${person.name} and there dog ${person.dogName}`
+    }
+    return greet(person)
+}
+
+console.log(greetPersonAndDog({
+    name: "Chris",
+    favNumber: 13,
+    dogName: "Blu",
+    streamerType: "partner",
+    currentTime() {
+        return new Date();
+    }
+}))
+
+// type
+
 
 // Classes
 
